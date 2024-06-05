b2ef28469498316f79baec32d2432e403d26bd6e
BrunoLezama <brunolezamamendez18@gmail.com>
2024-06-04 22:59:52 -0600

    configuracion de archivos estaticos y de medios

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path("blog/",include("blog.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+
+ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
