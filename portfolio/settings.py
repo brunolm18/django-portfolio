1760b6a1d5009b46a3aae54c6e2cff686875fa46
BrunoLezama <brunolezamamendez18@gmail.com>
2024-06-06 07:33:35 -0600

    cambios en el media

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,5 +81,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
