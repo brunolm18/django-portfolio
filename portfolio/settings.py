408aff52a29434259c5b09a88e35a22d14c6b51f
BrunoLezama <brunolezamamendez18@gmail.com>
2024-06-05 23:30:09 -0600

    añadiendo el dir

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,6 +76,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
