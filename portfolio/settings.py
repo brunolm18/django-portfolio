43f280faf4e7a23022bb5d74511d9b1869dcf685
BrunoLezama <brunolezamamendez18@gmail.com>
2024-06-05 20:14:33 -0600

    trabajando con solo sqlite3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
